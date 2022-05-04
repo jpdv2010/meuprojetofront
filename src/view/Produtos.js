94656f057ded0cb91af9cbbc27bd1bf95857703d
joao.davi <joao.davi@sankhya.com.br>
2022-05-04 11:00:13 -0300

    espacamento

--- src/view/Produtos.js
+++ src/view/Produtos.js
@@ -8,26 +8,26 @@ const Produtos = () => {
 
     useEffect(() => {
         const token = localStorage.getItem('access-token');
-        
+
         let headers = {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         };
 
-        axios.get("http://localhost:8480/api/produtos",headers).then((res) => {
+        axios.get("http://localhost:8480/api/produtos", headers).then((res) => {
             console.log(res);
             setProdutos(res.data);
         });
 
-    },[]);
-    return(
+    }, []);
+    return (
         <div>
             <Col xl={12}>
-            {produtos != null && produtos.length > 0?
-            <DinamicTable data={produtos} title={"Produtos"} smalTitle={"products"} />
-            : null}
-          </Col>
+                {produtos != null && produtos.length > 0 ?
+                    <DinamicTable data={produtos} title={"Produtos"} smalTitle={"products"} />
+                    : null}
+            </Col>
         </div>
     );
 }
